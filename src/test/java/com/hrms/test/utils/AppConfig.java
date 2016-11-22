eb1c620942e6796e034abb7befc8d931e85fe83d
sivan123 <sivaprasad.ps@gmail.com>
2016-11-23 00:11:01 +0800

    Fixes and clean up

--- src/test/java/com/hrms/test/utils/AppConfig.java
+++ src/test/java/com/hrms/test/utils/AppConfig.java
@@ -15,21 +15,13 @@ public class AppConfig {
     public static String APP_START_URL;
 
     static {
-        Configurations configs = new Configurations();
         try {
             Properties properties = new Properties();
             properties.load(properties.getClass().getResource("/config/application.properties").openStream());
-            //InputStream inputStream = AppConfig.class.getClassLoader().getResourceAsStream("application.properties");
-//            Configuration config = configs.properties("/config/application.properties");
-//            Properties properties = new Properties();
-//            properties.load(inputStream);
 
             BROWSER=properties.getProperty("browser");
             DRIVER_PATH=properties.getProperty("driverpath");
             APP_START_URL=properties.getProperty("appstarturl");
-//            BROWSER=config.getString("browser");
-//            DRIVER_PATH=config.getString("driverpath");
-//            APP_START_URL=config.getString("appstarturl");
 
         } catch (Exception e) {
             System.err.println("Unable to read configuration");
